1d692af530cfe1e409af886aa52956c88eabce0a
Abram Shook <ajshook77@gmail.com>
2022-06-27 12:43:52 -0500

    commit tree 1728be9daa6c26b956fa645d283c366ae77c3d8c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.where(first_name:params[:first_name], last_name:params[:last_name])
     render json: students
+  end
+
+  def show
+   student = Student.find(params[:id])
+  # Student.second
+  render json: student
   end
 
 end
